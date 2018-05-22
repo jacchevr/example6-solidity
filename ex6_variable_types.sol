1c7da203fe769da485fa0c76cbc9e31eafede234
Jacques Chevrier <34470570+jacchevr@users.noreply.github.com>
2018-05-22 10:56:47 -0600

    Update ex6_variable_types.sol

--- ex6_variable_types.sol
+++ ex6_variable_types.sol
@@ -1,1 +1,19 @@
-# example6-solidity
+pragma solidity ^0.4.18;
+
+contract ValueTypes {
+    function testIntegerDivision() public pure returns (uint256) {
+        uint varOne = 5;
+        uint varTwo = 2;
+        return varOne/varTwo;
+    }
+    
+    function testOverflow() public pure returns (uint256) {
+        uint varOne;
+        return varOne-1;
+    }
+    
+    function getThisBalance() public view returns (uint256) {
+        address thisContractAddress = this;
+        return thisContractAddress.balance;
+    }
+}
